2c079087fa4b921b4d4bfd1254fd8c049ec469a6
Humayun Kabir <humayun.kabir@ssd-tech.io>
2023-07-07 14:14:18 +0600

    initial commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+//Import bootstrap and bootstrapVue css files (Order is important)
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import BootstrapVue, { IconsPlugin } from "bootstrap-vue";
+
+//Make both available through the project
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
 Vue.config.productionTip = false;
 
 new Vue({
